648479301aa09fe87bb197d46dede40965047b6b
nidhalyahmadi <nidhal.yahmadi@esprit.tn>
2021-03-20 22:35:19 +0100

    frontResultat

---
+++ footTunisie1/src/edu/db3a4/gui2/ResultatfrontController.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.db3a4.gui2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author nidha
+ */
+public class ResultatfrontController implements Initializable {
+
+    @FXML
+    private AnchorPane MenuAnchorPane;
+    @FXML
+    private Pane sideTransparentPane;
+    @FXML
+    private Pane sideBorderPane;
+    @FXML
+    private Pane hoverPane;
+    @FXML
+    private Pane topTransparentPane;
+    @FXML
+    private ImageView backimg;
+    @FXML
+    private Pane blackPane11;
+    @FXML
+    private Pane hexPlane1;
+    @FXML
+    private Pane hexPlane2;
+    @FXML
+    private Pane hexPlane3;
+    @FXML
+    private Pane hexPlane;
+    @FXML
+    private Pane topBorderPane;
+    @FXML
+    private Pane blackPane;
+    @FXML
+    private Pane blackPane1;
+    @FXML
+    private ImageView topimage;
+    @FXML
+    private Pane topBlackTab;
+    @FXML
+    private Rectangle rectanimB;
+    @FXML
+    private BorderPane borderMenu;
+    @FXML
+    private AnchorPane topane;
+    @FXML
+    private Pane sideBarBorder;
+    @FXML
+    private Pane sideUp;
+    @FXML
+    private Pane sideDown;
+    @FXML
+    private AnchorPane sidePane;
+    @FXML
+    private VBox sidePaneVbox;
+    @FXML
+    private HBox status;
+    @FXML
+    private Text t1;
+    @FXML
+    private HBox myTeam;
+    @FXML
+    private Text t2;
+    @FXML
+    private HBox points;
+    @FXML
+    private Text t3;
+    @FXML
+    private HBox transfers;
+    @FXML
+    private Text t4;
+    @FXML
+    private HBox leagues;
+    @FXML
+    private Text t5;
+    @FXML
+    private HBox fixtures;
+    @FXML
+    private Text t6;
+    @FXML
+    private HBox statistics;
+    @FXML
+    private Text t7;
+    @FXML
+    private HBox rules;
+    @FXML
+    private Text t8;
+    @FXML
+    private ImageView fantasyIcon;
+    @FXML
+    private Button btnResize;
+    @FXML
+    private AnchorPane topane1;
+    @FXML
+    private Text HierText;
+    @FXML
+    private Button logout;
+    @FXML
+    private HBox topMenu;
+    @FXML
+    private Button btnThemes1;
+    @FXML
+    private Text ccost;
+    @FXML
+    private Text psel;
+    @FXML
+    private Text cgw;
+    @FXML
+    private Text cgwp;
+    @FXML
+    private Text tpoints;
+    @FXML
+    private Text bp;
+    @FXML
+    private Text agw;
+    @FXML
+    private Text agwo;
+    @FXML
+    private Text aagw;
+    @FXML
+    private Text aagwo;
+    @FXML
+    private TableView<?> tableHistory;
+    @FXML
+    private TableColumn<?, ?> gw;
+    @FXML
+    private TableColumn<?, ?> opponent;
+    @FXML
+    private TableColumn<?, ?> pts;
+    @FXML
+    private TableColumn<?, ?> mp;
+    @FXML
+    private TableColumn<?, ?> gs;
+    @FXML
+    private TableColumn<?, ?> a;
+    @FXML
+    private TableColumn<?, ?> cs;
+    @FXML
+    private TableColumn<?, ?> gc;
+    @FXML
+    private TableColumn<?, ?> og;
+    @FXML
+    private TableColumn<?, ?> rc;
+    @FXML
+    private TableColumn<?, ?> yc;
+    @FXML
+    private TableColumn<?, ?> pm;
+    @FXML
+    private TableColumn<?, ?> ps;
+    @FXML
+    private TableColumn<?, ?> s;
+    @FXML
+    private TableColumn<?, ?> b;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleMouseEvent(MouseEvent event) {
+    }
+
+    @FXML
+    private void test(MouseEvent event) {
+    }
+
+    @FXML
+    private void enterResize(MouseEvent event) {
+    }
+    
+}
