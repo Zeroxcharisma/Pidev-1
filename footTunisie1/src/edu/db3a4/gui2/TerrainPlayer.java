906b56d293f777e50f7515da3b8aa0c58b24265d
mahdikha <mahdi.khalsi@esprit.tn>
2021-03-23 14:15:55 +0100

    VideoPLayerTerrain

---
+++ footTunisie1/src/edu/db3a4/gui2/TerrainPlayer.java
@@ -0,0 +1,52 @@
+package edu.db3a4.gui2;
+
+
+
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ammar
+ */
+public class TerrainPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TerrainPlayer.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Media Player");
+        
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getClickCount() ==2){
+                    stage.setFullScreen(true);
+                }
+            }
+        });
+        
+        
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
